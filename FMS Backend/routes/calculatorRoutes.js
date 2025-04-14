d8556417379b059bf7bc651a95ae088c25e27614
Shubh Gupta <shubh.etw22@gmail.com>
2025-04-14 23:41:00 +0530

    Delete FMS Backend directory

--- FMS Backend/routes/calculatorRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const calculatorController = require('../controllers/calculatorController');
-const authMiddleware = require('../middlewares/authMiddleware');
-
-router.post('/budget', authMiddleware, calculatorController.calculateBudget);
-
-module.exports = router;
